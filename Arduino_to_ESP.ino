923f8ded1bb55a348beb9d7f5fee5c8666527bda
OliMarquez <121472005+OliMarquez@users.noreply.github.com>
2023-11-06 19:21:56 -0500

    Update Arduino_to_ESP.ino

--- Arduino_to_ESP.ino
+++ Arduino_to_ESP.ino
@@ -13,33 +13,54 @@
   const int airValue = 620; // Calibrate these two values
   const int waterValue = 310;
     
+#include "SPI.h"
+#include "Adafruit_GFX.h"
+#include "Adafruit_ILI9341.h"
+
+// For the Adafruit shield, these are the default.
+#define TFT_DC 9
+#define TFT_CS 53
+
+// Use hardware SPI (on Uno, #13, #12, #11) and the above for CS/DC
+Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC);
+// If using the breakout, change pins as desired
+//Adafruit_ILI9341 tft = Adafruit_ILI9341(TFT_CS, TFT_DC, TFT_MOSI, TFT_CLK, TFT_RST, TFT_MISO);
+
+
 // Setting Pins
   #define thermistor A0
   #define photoRes A1
   #define capSoilSensor1 A2
   #define capSoilSensor2 A3
-  #define capSoilSensor3 A6 //A4
-  #define capSoilSensor4 A7 //A5
+  #define capSoilSensor3 A6 
+  #define capSoilSensor4 A7
   #define floatSwitch25 5
   #define floatSwitch50 6
   #define floatSwitch75 7
   #define floatSwitch100 8
+  #define pushButton 10
+
 
 // Sets Up LCD & Pins
   void setup() {
     lcd.init(); //Initlaize LCD
     lcd.backlight(); //Glow
+    pinMode (pushButton, INPUT);  //button is an input
     pinMode(floatSwitch25, INPUT_PULLUP);
     pinMode(floatSwitch50, INPUT_PULLUP);
     pinMode(floatSwitch75, INPUT_PULLUP);
     pinMode(floatSwitch100, INPUT_PULLUP);
-    Serial.begin(9600);
+    Serial.begin(9600); //Start Serial Monitor
+     tft.begin();
     
   }
 
 // Calls Functions Every 10 Seconds
   void loop()
   {
+    testText();
+    
+
     setClock();
     if(seconds == 10){
       collectedData();
@@ -109,12 +130,42 @@
     capSoilValue2 = random(310,621); //analogRead(A3);
     capSoilValue3 = random(310,621); //analogRead(A4);
     capSoilValue4 = random(310,621); //analogRead(A5);
+    //Turn Soil Moisture Value To %
     soilMoisture1 = map(capSoilValue1, airValue, waterValue, 0, 100);
     soilMoisture2 = map(capSoilValue2, airValue, waterValue, 0, 100);    
     soilMoisture3 = map(capSoilValue3, airValue, waterValue, 0, 100);    
     soilMoisture4 = map(capSoilValue4, airValue, waterValue, 0, 100);    
+
+
+    
   }
 
+unsigned long testText() {
+  tft.fillScreen(ILI9341_BLACK);
+  unsigned long start = micros();
+  tft.setCursor(0, 0);
+  tft.setTextColor(ILI9341_WHITE);  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ILI9341_YELLOW); tft.setTextSize(2);
+  tft.println(1234.56);
+  tft.setTextColor(ILI9341_RED);    tft.setTextSize(3);
+  tft.println(0xDEADBEEF, HEX);
+  tft.println();
+  tft.setTextColor(ILI9341_GREEN);
+  tft.setTextSize(5);
+  tft.println(soilMoisture2);
+  tft.setTextSize(2);
+  tft.println("I implore thee, to join Robotics");
+  tft.setTextSize(5);
+  tft.println("my foonting turlingdromes.");
+  tft.println("And hooptiously drangle me");
+  tft.println("with crinkly bindlewurdles,");
+  tft.println("Or I will rend thee");
+  tft.println("in the gobberwarts");
+  tft.println("with my blurglecruncheon,");
+  tft.println("see if I don't!");
+  return micros() - start;
+}
 // Prints Out Data To Send to ESP (Ex: <10.2, 11.89, 59.00>)
   void printData(){
     Serial.print("<");
